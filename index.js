da9351258fecc655775b7ab4550d06e1b45e4787
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-24 16:56:01 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Add your code here
+
+
+// Add your code here
+
+/*code along practice
+
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  // method: "POST" is missing from the object below
+  const configurationObject = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });*/
+
+    function submitData (name, email) {
+        return fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                name,
+                email,
+            }),
+
+        })
+        .then((response)=>response.json())
+        .then(function (storeusers){
+            return document.body.innerHTML = storeusers["id"]
+        })
+        .catch(function (error){
+            alert(error.message);
+            console.log(error.message);
+            document.body.innerHTML = error.message;
+        })
+    } 
+
+   
